7fd7734348bf44b0e850ca31a877bf1942aeaeb0
anil <anilk11432@gmail.com>
2017-10-30 04:07:18 -0500

    few changes

--- server/server.js
+++ server/server.js
@@ -4,10 +4,16 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('serveris up on port 3000');
+app.get('/', (req, res) => {
+    res.send('hi');
+})
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
 });
