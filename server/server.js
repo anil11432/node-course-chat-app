121b66b979af5818125d00d610277e73b11d7ccb
anil <anilk11432@gmail.com>
2017-10-31 07:11:36 -0500

    broadcating done

--- server/server.js
+++ server/server.js
@@ -16,10 +16,39 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) =>{
     console.log('connected to the client');
 
+    socket.emit('newMessage',{
+        from: 'admin',
+        text: 'welcome to chat',
+        createdat: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'new user joined',
+        createdat: new Date().getTime()
+    });
+
+    socket.on('createMessage', (message) =>{
+        console.log('this is from client', message);
+
+        io.emit('newMessage',{
+            from: message.from,
+            text: message.text,  
+            createdon: new Date().getTime()
+        });
+
+        // socket.broadcast.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,  
+        //     createdon: new Date().getTime()
+        // });
+    });
+
     socket.on('disconnect', (socket) =>{
-        console.log('disconnected from the client');
+        console.log('disconnected from the client'); 
     });
 });
+
 
 server.listen(port, () => {
     console.log(`server is up on port ${port}`);
