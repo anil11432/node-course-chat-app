d05eac05ac5857ab655d88e83b530f2c2a6dafa1
anil <anilk11432@gmail.com>
2017-11-02 16:08:58 +0530

    new files updated

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+var socket = io();
+socket.on('connect', function() {
+    console.log('connected to server');
+});
+
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
+}); 
+
+socket.on('newMessage', function(message){
+    console.log('new message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from : 'user',
+        text: jQuery('[name=message]').val()
+    }, function(){
+
+    });
+});
