1cffff80abdbe556d4c43ec9d515493d9edef3a7
anil <anilk11432@gmail.com>
2017-11-06 17:23:18 +0530

    messege sending updated

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,10 @@
 var socket = io();
+
+var conn;
+
 socket.on('connect', function() {
     console.log('connected to server');
+    conn = Math.floor(Math.random() * 1000);
 });
 
 socket.on('disconnect', function() {
@@ -9,10 +13,33 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message){
     console.log('new message', message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    socket.emit('translate', {
+        text: message.text,
+        from: message.lang,
+        to: jQuery('#lang').val(),
+        id : conn
+    });
+    // var li = jQuery('<li></li>');
+    // li.text(`${message.from}: ${message.text} : ${message.trans}`);
+    // jQuery('#messages').append(li);
+});
 
-    jQuery('#messages').append(li);
+socket.on('translatedMessage', function(message) {
+    console.log('translated message' + message.translated);
+    console.log(message.conn);
+    if (message.id == conn) {
+        var li = jQuery('<li></li>');
+        li.text(`${message.translated}`);
+        jQuery('#messages').append(li);
+        console.log(conn);
+    }
+    // else{
+    //     var li = jQuery('<li></li>');
+    //     li.text(`${message.original}`);
+    //     jQuery('#messages').append(li);
+    // } 
+    ;
+    
 });
 
 jQuery('#message-form').on('submit', function(e){
@@ -20,8 +47,12 @@ jQuery('#message-form').on('submit', function(e){
 
     socket.emit('createMessage', {
         from : 'user',
-        text: jQuery('[name=message]').val()
+        text: jQuery('[name=message]').val(),
+        lang: jQuery('#lang').val(),
+        id : conn
     }, function(){
 
     });
 });
+
+
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
     mongoose.Promise = global.Promise;
 const publicPath = path.join(__dirname, '../public');
 
+const translate = require('google-translate-api');
+
 const port = process.env.PORT || 27017;
 
 var app = express();
@@ -31,6 +33,9 @@ io.on('connection',(socket) =>{
         createdat: new Date().getTime()
     });
 
+        
+
+
     socket.on('createMessage', (message, callback) =>{
         console.log('this is from client', message);
 
@@ -38,7 +43,8 @@ io.on('connection',(socket) =>{
 
         let messageContent = new msg({
             from: message.from,
-            text: message.text
+            text: message.text,
+            lang: message.lang,
         });
 
         messageContent.save().then((doc) => {
@@ -47,20 +53,68 @@ io.on('connection',(socket) =>{
             console.log('unable to save  data');
         });
 
-
         io.emit('newMessage',{
             from: message.from,
-            text: message.text,  
-            createdon: new Date().getTime()
+            lang: message.lang,  
+            createdon: new Date().getTime(),
+            text: message.text,
+            id : message.id
         });
 
-        callback('this is from the server');
+
+
+
+
+        // translate(message.text, {from: 'en', to: 'ca'}).then(res => {
+        //     console.log(res.text);
+        //     //=> Ik spreek Nederlands! 
+        //     console.log(res.from.text.autoCorrected);
+        //     //=> true 
+        //     console.log(res.from.text.value);
+        //     //=> I [speak] Dutch! 
+        //     console.log(res.from.text.didYouMean);
+        //     //=> false
+        //     io.emit('newMessage',{
+        //         from: message.from,
+        //         trans: res.text,  
+        //         createdon: new Date().getTime(),
+        //         text: message.text
+        //     }); 
+        // }).catch(err => {
+        //     console.error(err);
+        // });
+
+
+        // callback('this is from the server');
 
         // socket.broadcast.emit('newMessage',{
         //     from: message.from,
         //     text: message.text,  
         //     createdon: new Date().getTime()
         // });
+    });
+
+    socket.on('translate', (message, callback) => {
+        console.log(`to be translate from ${message.from} to ${message.to}`);
+        translate(message.text, {from: message.from, to: message.to}).then(res => {
+            console.log(res.text);
+            //=> Ik spreek Nederlands! 
+            console.log(res.from.text.autoCorrected);
+            //=> true 
+            console.log(res.from.text.value);
+            //=> I [speak] Dutch! 
+            console.log(res.from.text.didYouMean);
+            //=> false
+            io.emit('translatedMessage',{
+                original: message.text,
+                translated: res.text,
+                from: message.from,
+                to: message.to,
+                id : message.id
+            });
+        }).catch(err => {
+            console.error(err);
+        });
     });
 
     socket.on('disconnect', (socket) =>{
